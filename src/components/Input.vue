c015e0a9872dd4004275b2b77d8ca54b6b75f023
herisusantoarismann <heri.s.arisman@gmail.com>
2022-11-16 09:09:00 +0700

    Add input component

---
+++ src/components/Input.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="mb-4">
+    <input
+      :type="type"
+      class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-[#4BB543] focus:outline-none"
+      :placeholder="name"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputComponent',
+  props: ['name', 'type'],
+};
+</script>
+
+<style></style>
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -3,48 +3,29 @@
     class="min-h-screen flex flex-col gap-4 mx-6 my-8 md:w-2/4 md:m-auto lg:w-2/6 xl:w-1/6"
   >
     <div class="text-center">
-      <img
-        class="mx-auto w-48"
-        src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
-        alt="logo"
-      />
-      <h4 class="text-xl font-semibold mt-1 pb-1">We are The Lotus Team</h4>
+      <img class="mx-auto w-48" src="../assets/logo.png" alt="logo" />
+      <h4 class="text-xl font-semibold mt-1 pb-1">Login</h4>
     </div>
     <form>
-      <div class="mb-4">
-        <input
-          type="email"
-          class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-          id="exampleFormControlInput1"
-          placeholder="Email"
-        />
-      </div>
-      <div class="mb-4">
-        <input
-          type="password"
-          class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-          id="exampleFormControlInput1"
-          placeholder="Password"
-        />
-      </div>
+      <Input name="Email" type="email" />
+      <Input name="Password" type="password" />
       <div class="text-center pt-1 pb-1">
-        <button
-          class="inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded shadow-md bg-[#013c44] hover:bg-blue-700 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3"
-          type="button"
-          data-mdb-ripple="true"
-          data-mdb-ripple-color="light"
-        >
-          Log in
-        </button>
+        <Button text="Login" :block="true" />
       </div>
-      <p class="text-sm text-center">Don`t have an account? Register here.</p>
+      <p class="mt-4 text-sm text-center">
+        Don`t have an account? Register here.
+      </p>
     </form>
   </div>
 </template>
 
 <script>
+import Input from '../components/Input.vue';
+import Button from '../components/Button.vue';
+
 export default {
   name: 'LoginPage',
+  components: { Input, Button },
 };
 </script>
 
